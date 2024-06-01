685cf0ef275de9fb7e864917483209ee6e331a4b
muntasir23 <muntasiraahmed3@gmail.com>
2024-06-01 08:33:53 -0700

    nw

--- app/expense/page.js
+++ app/expense/page.js
@@ -4,7 +4,7 @@ import ExpenseForm from "../components/ExpenseForm";
 export default function Expense() {
   return (
     <>
-      <div className="w-full lg:w-[400px] md:[400px] h-[100vh] flex flex-col items-center justify-center border-2">
+      <div className="w-full lg:w-[400px] md:w-[400px] h-[100vh] flex flex-col items-center justify-center border-2">
         <h1 className="mb-5 text">
           Fill out all the details to save your <strong className="text-lime-500">expense</strong>
         </h1>
--- app/layout.js
+++ app/layout.js
@@ -21,12 +21,12 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <div>
-          <header className=" flex items-center justify-center sticky top-0 w-[full] lg:w-[400px] z-100000000 backdrop-blur-[15px] p-2">
+          <header className=" flex items-center justify-center sticky top-0 w-full lg:w-[400px] z-100000000 backdrop-blur-[15px] p-2">
             <Link href='/'>
             <Image alt="" src={logo} className="w-[90px]" />
             </Link>
           </header>
-          <nav className="flex justify-between items-center px-3 py-3 fixed bottom-0 lg:w-[400px] z-100000000 bg-zinc-100 w-[100%]">
+          <nav className="flex justify-between items-center px-3 py-3 fixed bottom-0 lg:w-[400px] z-100000000 bg-zinc-100 w-full md:w-[400px]">
             <Button>
               <p className="w-[30px] h-[30px] rounded-full bg-gray-900 text-lime-300 flex justify-center items-center">
                 <FaPlus />
